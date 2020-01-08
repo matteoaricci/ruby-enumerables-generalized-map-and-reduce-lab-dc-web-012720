65b2904a807bef043a9b1e655f9961c6b7ebc67f
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-08 17:02:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(source)
+  i = 0
+  arr = []
+
+    while i < source.length do
+    arr.push(yield(source[i]))
+    i += 1
+    end
+  arr
+end
+
+def reduce(source, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = source[0]
+    i = 1
+  end 
+
+  while i < source.length
+    sum = yield(sum, source[i])
+    i += 1
+  end
+  sum
+end
